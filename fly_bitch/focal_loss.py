1f0f85c0e85d46c328187a41f9d09e262fbfe6b2
Alex Chi <iskyzh@gmail.com>
2021-05-27 13:52:59 +0800

    fmt

--- fly_bitch/focal_loss.py
+++ fly_bitch/focal_loss.py
@@ -2,12 +2,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class FocalLoss(nn.Module):
     def __init__(self, alpha=1, gamma=2):
         super(FocalLoss, self).__init__()
         self.alpha = alpha
         self.gamma = gamma
-    
+
     def forward(self, inputs, targets):
         # Assert: The inputs have been applied on Sigmoid !!!
         # It is equivalent to nn.BCELoss
@@ -19,12 +20,13 @@ class FocalLoss(nn.Module):
         F_loss = self.alpha * ((1-pt)**self.gamma) * BCE_loss
         return torch.mean(F_loss)
 
+
 def main(argv):
     from torch import autograd
-    inputs = autograd.Variable(torch.randn(3,3), requires_grad=True)
+    inputs = autograd.Variable(torch.randn(3, 3), requires_grad=True)
     m = nn.Sigmoid()
     print(m(inputs))
-    target = torch.FloatTensor([[0,1,1], [1,1,1], [0,0,0]])
+    target = torch.FloatTensor([[0, 1, 1], [1, 1, 1], [0, 0, 0]])
     loss_fn = FocalLoss()
     loss = loss_fn(m(inputs), target)
     print(loss)
