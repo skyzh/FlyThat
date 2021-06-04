26b56a653024317cfb57f5a7f283a72a14742d8a
Leavrth <Legenull@gmail.com>
2021-06-04 00:35:31 +0000

    fea[:-2] + agg_666

--- fly_bitch/feature_layer.py
+++ fly_bitch/feature_layer.py
@@ -8,7 +8,10 @@ class FeatureExtractionLayer(nn.Module):
     def __init__(self):
         super(FeatureExtractionLayer, self).__init__()
         origin = torchvision.models.resnet18(pretrained=True)
-        self.features = origin
+        #self.features = origin
+        self.features = nn.Sequential(
+            *list(origin.children())[:-2]
+        )
 
     def forward(self, x):
         result = self.features(x)
